afcca320acbaf6416cb2cfa000e37fa3eae483e3
Aleksei Naumov <naumov78@gmail.com>
2017-01-11 12:02:01 -0500

    update applitation.rb

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,12 @@ module Quora
 
     config.paperclip_defaults = {
       :storage => :s3,
-      :s3_host_name => 's3.amazonaws.com',
+      # :s3_host_name => 's3.amazonaws.com',
       :s3_credentials => {
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
-        :s3_region => ENV["s3_region"]
+        :region => ENV["s3_region"]
       }
     }
 
