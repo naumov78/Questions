bc25c641aabdb880ad7a9067a93452a67a626622
Aleksei Naumov <naumov78@gmail.com>
2017-02-10 15:30:05 -0500

    remove logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-// import rootReducer from '../reducers/root_reducer';
+// import logger from 'redux-logger';
+import rootReducer from '../reducers/root_reducer';
 
 
 
