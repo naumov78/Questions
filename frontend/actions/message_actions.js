1af672a11ea2f008731c8c52b8b96b0646e72515
Aleksei Naumov <naumov78@gmail.com>
2017-03-07 23:27:16 -0500

    created actions for messages

---
+++ frontend/actions/message_actions.js
@@ -0,0 +1,41 @@
+import * as APIUtil from '../util/messagess_api_util';
+
+
+export const RECEIVE_MESSAGE = "RECEIVE_MESSAGE";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+
+export const receiveMessage = (message) => {
+  return ({
+    type: RECEIVE_MESSAGE,
+    question: question
+  });
+}
+
+
+export const receiveErrors = (errors) => {
+  return ({
+    type: RECEIVE_ERRORS,
+    errors
+  });
+}
+
+
+export const createMessage = (message) => {
+  return (dispatch) => {
+    return APIUtil.createMessage(message).then((result) => {
+      return dispatch(receiveMessage(result));
+      },
+      (errors) => {
+        return dispatch(receiveErrors(errors));
+      });
+  }
+}
+
+export const fetchMessage = (id) => {
+  return (dispatch) => {
+    return APIUtil.fetchMessage(id).then((result) => {
+      return dispatch(receiveMessage(result))
+    });
+  }
+}
